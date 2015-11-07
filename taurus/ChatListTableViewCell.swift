1e26e5dd45563b513b34a9f3ca72f2fd223d753d
Venkatesh Muthukrishnan <venkatesh9222@gmail.com>
2015-11-07 17:03:47 -0500

    suma

---
+++ taurus/ChatListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ChatListTableViewCell.swift
+//  taurus
+//
+//  Created by Venkatesh Muthukrishnan on 11/7/15.
+//  Copyright © 2015 Srinivasan Sundaramoorthy. All rights reserved.
+//
+
+import UIKit
+
+class ChatListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ChatName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
