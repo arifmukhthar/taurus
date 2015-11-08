7833fa2d0c9d9e28df04e4e1c7d2a83d3502e2ae
Ravi <Annie@cpxpryde.resource.campus.njit.edu>
2015-11-07 20:51:56 -0500

    asdf

--- taurus/ViewController.swift
+++ taurus/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var loginSuccess = false
+    @IBOutlet weak var txtUname: UITextField!
+    @IBOutlet weak var txtPwd: UITextField!
+    @IBAction func checkUserPass(sender: AnyObject) {
+        getJson("https://web.njit.edu/~rb454/login.php",uname: txtUname.text!)
+        if !loginSuccess
+        {
+            let alert = UIAlertController(title: "User Name or Password is wrong", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,5 +29,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-}
+    func getJson(url: String,uname: String)
+    {
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        request.HTTPMethod = "POST"
+        
+        let params = "email='\(uname)''"
+        print(params)
+        request.HTTPBody = params.dataUsingEncoding(NSUTF8StringEncoding)
+        let task1 = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {data,response, error -> Void in
+            print("data: \(data)")
+            
+            do{
+                var i = 0
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSArray
+                //print(json)
+                for _ in json! {
+                    let dd = json![i]
+                    let uname = dd["UserName"] as! String
+                    let pwd = dd["UserPwd"] as! String
+                    print("un: \(uname) pwd:\(pwd)")
+                   if self.txtUname.text == uname && self.txtPwd.text == pwd
+                   {
+                        self.loginSuccess = true
+                    }
+                    i++
+                    
+                }
+                dispatch_async(dispatch_get_main_queue(), {
+                    
+                });
+
+                print(json)
+            }catch _ as NSError{
+                
+            }
+        })
+        task1.resume()
+
+
+    }
+    
+    }
 
